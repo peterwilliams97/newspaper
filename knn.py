f9b8dc1e494f590a319d7020de6701cf02d9b226
peterwilliams97 <peter.williams.97@gmail.com>
2011-01-30 14:27:45 +1100

    made knn naming more consistent

--- knn.py
+++ knn.py
@@ -10,31 +10,31 @@ Created on 29/01/2011
 import  math, pickle
 import numpy as np
 
-def get_knn(k, data, data_class, inputs):
+def get_knn(k, training_data, training_data_class, test_data):
     """ Naive implementation of k nearest neighbors
         k: number of nearest neighbors
-        data: training data instances
-        data_class: classes of each training data instance
-        inputs: inputs to classify
+        training_data: training training_data instances
+        training_data_class: classes of each training training_data instance
+        test_data: test_data to classify
         returns: classes of each input instance
     """
-    num_inputs = np.shape(inputs)[0]
+    num_inputs = np.shape(test_data)[0]
     if False:
         print 'num_inputs =', num_inputs
-        print 'data =', data
-        print 'data_class =', data_class
-        print 'inputs =', inputs
+        print 'training_data =', training_data
+        print 'training_data_class =', training_data_class
+        print 'test_data =', test_data
     closest = np.zeros(num_inputs)
 
     for n in range(num_inputs):
-        distances = np.sqrt(np.sum((data - inputs[n,:])**2, axis = 1))
-        #print 'i =', inputs[n,:]
+        distances = np.sqrt(np.sum((training_data - test_data[n,:])**2, axis = 1))
+        #print 'i =', test_data[n,:]
         #print 'd =', distances
     
         indices = np.argsort(distances, axis = 0)
         #print 'indices =', indices
         
-        classes = data_class[indices[:k]]
+        classes = training_data_class[indices[:k]]
         #print 'classes =', classes
     
         classes = np.unique(classes)
@@ -45,7 +45,7 @@ def get_knn(k, data, data_class, inputs):
             #print 'x'*10
             counts = np.zeros(max(classes) + 1)
             for i in range(k):
-                counts[data_class[indices[i]]] += 1
+                counts[training_data_class[indices[i]]] += 1
             #print 'counts =', counts
             closest[n] = np.argmax(counts)
 
@@ -60,77 +60,77 @@ def blend(a,b,r):
     return (r*a + b)/(1.0 + r)
     
 def make_data(num_instances, num_classes, num_dimensions, max_delta):
-    """ Make classification test data 
+    """ Make classification test training_data 
         num_instances: number of training samples
         num_classes: number of different classes for samples
-        num_dimensions: number of attributes in data
+        num_dimensions: number of attributes in training_data
         max_delta: each attribute of each training sample is in range 
                     [centroid-max_deta,centroid+max_delta]
-        returns: data,data_class,inputs,inputs_class
-            data: training samples
-            data_class: classes of training samples
-            inputs: test samples
-            inputs_class: classes of test samples
+        returns: training_data,training_data_class,test_data,test_data_class
+            training_data: training samples
+            training_data_class: classes of training samples
+            test_data: test samples
+            test_data_class: classes of test samples
     """
     verbose = False
     
     print 'make_data', num_instances, num_classes, num_dimensions
     instances_per_class = int(math.ceil(num_instances/num_classes))
     centroids = [[i+1] * num_dimensions for i in range(num_classes)]
-    data_class = []
-    data = []
+    training_data_class = []
+    training_data = []
     for i in range(num_instances):
         clazz = i//instances_per_class
         cent = centroids[clazz]
         val = [cent[j] + rand(max_delta) for j in range(num_dimensions)]
-        data_class.append(clazz)
-        data.append(val)
-    inputs = sorted(centroids) 
+        training_data_class.append(clazz)
+        training_data.append(val)
+    test_data = sorted(centroids) 
     ratio = 4.0
     for k in range(1):
-        #print len(inputs), inputs
-        for i in range(1,len(inputs)):
-            new_els = [[blend(inputs[i-1][j], inputs[i][j], ratio) for j in range(num_dimensions)],
-                       [blend(inputs[i-1][j], inputs[i][j], 1.0/ratio) for j in range(num_dimensions)]]
+        #print len(test_data), test_data
+        for i in range(1,len(test_data)):
+            new_els = [[blend(test_data[i-1][j], test_data[i][j], ratio) for j in range(num_dimensions)],
+                       [blend(test_data[i-1][j], test_data[i][j], 1.0/ratio) for j in range(num_dimensions)]]
             #print 'new_els = ', new_els
-            inputs += new_els
-        inputs.sort()
+            test_data += new_els
+        test_data.sort()
     if verbose:
         print 'centroids', centroids
-        print 'inputs', len(inputs), inputs
-        print 'data', data
-        print 'data_class', data_class
-    inputs_class = [np.argmin(np.array([abs(inp[0] - cent[0]) for cent in centroids])) for inp in inputs]
+        print 'test_data', len(test_data), test_data
+        print 'training_data', training_data
+        print 'training_data_class', training_data_class
+    test_data_class = [np.argmin(np.array([abs(inp[0] - cent[0]) for cent in centroids])) for inp in test_data]
     if verbose:
-        for i,inp in enumerate(inputs):
-            print '%2d:'%i, [abs(inp[0] - cent[0]) for cent in centroids], inputs_class[i]
-    inputs_class = [np.argmin(np.array([abs(inp[0] - cent[0]) for cent in centroids])) for inp in inputs]
+        for i,inp in enumerate(test_data):
+            print '%2d:'%i, [abs(inp[0] - cent[0]) for cent in centroids], test_data_class[i]
+    test_data_class = [np.argmin(np.array([abs(inp[0] - cent[0]) for cent in centroids])) for inp in test_data]
     if verbose:
-        print 'inputs_class', inputs_class
+        print 'test_data_class', test_data_class
     #exit()
-    return np.array(data), np.array(data_class), np.array(inputs), np.array(inputs_class)
+    return np.array(training_data), np.array(training_data_class), np.array(test_data), np.array(test_data_class)
 
 def test_knn_on_sample(title, num_instances, num_classes, num_dimensions, max_delta):
     """ Test get_knn() by running it on some synthetic samples
         title: name of test
         num_instances: number of training samples
         num_classes: number of different classes for samples
-        num_dimensions: number of attributes in data
+        num_dimensions: number of attributes in training_data
         max_delta: each attribute of each training sample is in range 
                     [centroid-max_deta,centroid+max_delta]
         return: True iff all samples are classified correctly
     """
     print '#'*40
-    data,data_class,inputs,inputs_class = make_data(num_instances, num_classes, num_dimensions, max_delta)
+    training_data,training_data_class,test_data,test_data_class = make_data(num_instances, num_classes, num_dimensions, max_delta)
     print '='*40
     print 'num_instances, num_classes, num_dimensions, max_delta =', num_instances, num_classes, num_dimensions, max_delta
     for k in range(1, min(10,num_instances//num_classes)+1):
         print '-'*40 + ':', title, num_instances, num_classes, num_dimensions, max_delta 
-        closest = get_knn(k, data, data_class, inputs)
+        closest = get_knn(k, training_data, training_data_class, test_data)
         print 'k =', k
         print 'closest =', closest
-        print 'inputs_class =', inputs_class
-        matches = [closest[i] == inputs_class[i] for i in range(inputs_class.shape[0])]
+        print 'test_data_class =', test_data_class
+        matches = [closest[i] == test_data_class[i] for i in range(test_data_class.shape[0])]
         print 'matches =', matches
         if not all(matches):
             print 'mismatch!'
@@ -193,9 +193,9 @@ def test_knn():
             
 def test_knn0():
     if False:
-        data = np.array([[1,2,3],[3,1,2],[2,3,1],[4,5,6],[6,4,5],[5,6,4]])
-        data_class = np.array([0,0,0,1,1,1])
-        inputs = np.array([[2,2,2],[5,5,5],[3.4,3.5,3.6]])
+        training_data = np.array([[1,2,3],[3,1,2],[2,3,1],[4,5,6],[6,4,5],[5,6,4]])
+        training_data_class = np.array([0,0,0,1,1,1])
+        test_data = np.array([[2,2,2],[5,5,5],[3.4,3.5,3.6]])
          
     num_instances, num_classes, num_dimensions, max_delta = 35, 5, 3, 0.3
     num_instances, num_classes, num_dimensions, max_delta = 6, 2, 1, 0.1
@@ -203,15 +203,15 @@ def test_knn0():
     num_instances, num_classes, num_dimensions, max_delta = 35, 5, 1, 0.4
     #num_instances, num_classes, num_dimensions, max_delta = 10055, 5, 17, 0.4
     #num_instances, num_classes, num_dimensions, max_delta = 1090055, 15, 27, 0.49
-    data,data_class,inputs,inputs_class = make_data(num_instances, num_classes, num_dimensions, max_delta)
+    training_data,training_data_class,test_data,test_data_class = make_data(num_instances, num_classes, num_dimensions, max_delta)
    
     for k in range(1, min(10,num_instances//num_classes)+1):
         print '-'*20
-        closest = get_knn(k, data, data_class, inputs)
+        closest = get_knn(k, training_data, training_data_class, test_data)
         print 'k =', k
         print 'closest =', closest
-        print 'inputs_class =', inputs_class
-        matches = [closest[i] == inputs_class[i] for i in range(inputs_class.shape[0])]
+        print 'test_data_class =', test_data_class
+        matches = [closest[i] == test_data_class[i] for i in range(test_data_class.shape[0])]
         print 'matches =', matches
         if not all(matches):
             print 'mismatch!'
