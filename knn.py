cba74d08f677cf1c3c9479f50764dbaada947510
peterwilliams97 <peter.williams.97@gmail.com>
2011-01-30 14:23:40 +1100

    improved knn test case generation

--- knn.py
+++ knn.py
@@ -7,7 +7,7 @@ Created on 29/01/2011
 
 @author: peter
 """
-import random, math
+import  math, pickle
 import numpy as np
 
 def get_knn(k, data, data_class, inputs):
@@ -54,6 +54,10 @@ def get_knn(k, data, data_class, inputs):
 def rand(max_delta):
     """ Return a random number in range [-max_delta..max_delta] """
     return (np.random.random() - 0.5) * max_delta/0.5
+
+def blend(a,b,r):
+    """ Blend a and b with ratio r """
+    return (r*a + b)/(1.0 + r)
     
 def make_data(num_instances, num_classes, num_dimensions, max_delta):
     """ Make classification test data 
@@ -68,6 +72,7 @@ def make_data(num_instances, num_classes, num_dimensions, max_delta):
             inputs: test samples
             inputs_class: classes of test samples
     """
+    verbose = False
     
     print 'make_data', num_instances, num_classes, num_dimensions
     instances_per_class = int(math.ceil(num_instances/num_classes))
@@ -81,28 +86,33 @@ def make_data(num_instances, num_classes, num_dimensions, max_delta):
         data_class.append(clazz)
         data.append(val)
     inputs = sorted(centroids) 
-    for k in range(2):
-        print len(inputs), inputs
+    ratio = 4.0
+    for k in range(1):
+        #print len(inputs), inputs
         for i in range(1,len(inputs)):
-            inputs.append([(1.1*inputs[i-1][j] + 0.9*inputs[i][j])/2 for j in range(num_dimensions)])
+            new_els = [[blend(inputs[i-1][j], inputs[i][j], ratio) for j in range(num_dimensions)],
+                       [blend(inputs[i-1][j], inputs[i][j], 1.0/ratio) for j in range(num_dimensions)]]
+            #print 'new_els = ', new_els
+            inputs += new_els
         inputs.sort()
-    if False:
-        print centroids
-        print len(inputs), inputs
-        print data
-        print data_class
-        print '***', [[abs(inp[0] - cent[0]) for cent in centroids] for inp in inputs]
+    if verbose:
+        print 'centroids', centroids
+        print 'inputs', len(inputs), inputs
+        print 'data', data
+        print 'data_class', data_class
     inputs_class = [np.argmin(np.array([abs(inp[0] - cent[0]) for cent in centroids])) for inp in inputs]
-    if False:
+    if verbose:
         for i,inp in enumerate(inputs):
             print '%2d:'%i, [abs(inp[0] - cent[0]) for cent in centroids], inputs_class[i]
     inputs_class = [np.argmin(np.array([abs(inp[0] - cent[0]) for cent in centroids])) for inp in inputs]
-    print inputs_class
+    if verbose:
+        print 'inputs_class', inputs_class
     #exit()
     return np.array(data), np.array(data_class), np.array(inputs), np.array(inputs_class)
 
-def test_knn_on_sample(num_instances, num_classes, num_dimensions, max_delta):
+def test_knn_on_sample(title, num_instances, num_classes, num_dimensions, max_delta):
     """ Test get_knn() by running it on some synthetic samples
+        title: name of test
         num_instances: number of training samples
         num_classes: number of different classes for samples
         num_dimensions: number of attributes in data
@@ -115,7 +125,7 @@ def test_knn_on_sample(num_instances, num_classes, num_dimensions, max_delta):
     print '='*40
     print 'num_instances, num_classes, num_dimensions, max_delta =', num_instances, num_classes, num_dimensions, max_delta
     for k in range(1, min(10,num_instances//num_classes)+1):
-        print '-'*40
+        print '-'*40 + ':', title, num_instances, num_classes, num_dimensions, max_delta 
         closest = get_knn(k, data, data_class, inputs)
         print 'k =', k
         print 'closest =', closest
@@ -128,15 +138,58 @@ def test_knn_on_sample(num_instances, num_classes, num_dimensions, max_delta):
     
     return True
 
+def make_pickle_filename(name):
+    return 'knn.%s.pkl' % name
+
+def get_pickled_base(name):
+    f = open(make_pickle_filename(name), 'rb')
+    val = pickle.load(f)
+    f.close()
+    return val
+
+def set_pickled(name, val):
+    f = open(make_pickle_filename(name), 'wb')
+    val = pickle.dump(val,f)
+    f.close()
+
+def get_pickled(name, default):
+    try:
+        val = get_pickled_base(name)
+    except:
+        set_pickled(name, default)
+        val = get_pickled_base(name)
+    return val
+
 def test_knn():
-    test_settings = [(35, 5, 3, 0.3),
+    # Need to seed random number generator to give same result for each run
+    np.random.seed(111)
+    test_settings = [(4, 2, 50, 0.499),
+                     (4, 2, 2, 0.1),
+                     (35, 5, 3, 0.3),
                      ( 6, 2, 1, 0.1),
                      (35, 5, 1, 0.4),
                      (10055, 5, 17, 0.4),
-                     (15, 27, 0.49)]
+                     #(10055, 15, 27, 0.49)
+                     ]
+
+    title = 'base test' 
     for num_instances, num_classes, num_dimensions, max_delta in test_settings:
-        if not test_knn_on_sample(num_instances, num_classes, num_dimensions, max_delta):
+        if not test_knn_on_sample(title, num_instances, num_classes, num_dimensions, max_delta):
             exit()
+    
+    test_number = 1
+    # highest_max_delta convergeed to 0.324486403921 in previous testing
+    highest_max_delta = get_pickled('highest_max_delta', 0.4999)           
+    while True:
+        title = 'test %02d' % test_number
+        for num_instances, num_classes, num_dimensions, max_delta in test_settings:
+            while True:
+                if test_knn_on_sample(title,num_instances, num_classes, num_dimensions, highest_max_delta):
+                    break
+                highest_max_delta *= 0.99
+                set_pickled('highest_max_delta', highest_max_delta)
+        print '*** test:', test_number, ', highest_max_delta:', highest_max_delta 
+        test_number += 1
             
 def test_knn0():
     if False:
